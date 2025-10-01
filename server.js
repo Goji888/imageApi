95ca2136dc523f974b0fe74dfa822864153d73d4
Goji888 <goji888real@gmail.com>
2025-10-01 17:09:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sharp=require("sharp")
 const app=express()
 const PORT=process.env.PORT||3000
 
-app.get("/image-to-rgb",async(req,res)=>{
+app.get("/imageApi",async(req,res)=>{
   try{
     const imageUrl=req.query.url
     const size=parseInt(req.query.size)||100
@@ -27,3 +27,4 @@ app.get("/image-to-rgb",async(req,res)=>{
 })
 
 app.listen(PORT,()=>console.log(`✅ API running on port ${PORT}`))
+
